f800e63a48c368730695a357e2ca679c8402fb0b
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-03-24 18:52:18 +0530

    Max Consecutive Ones

---
+++ Arrays/Easy/maximumConsecutiveOnes.cpp
@@ -0,0 +1,24 @@
+// Leetcode 485. Max Consecutive Ones
+
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi = 0;
+        int cnt = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 1){
+                cnt++;
+                maxi = max(maxi, cnt);
+            }
+            else{
+                cnt = 0;
+            }
+        }
+        return maxi;
+    }
+};
