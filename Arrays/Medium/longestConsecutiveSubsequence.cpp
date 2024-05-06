26015e9461ce317f34c98d8d08e47d7365849b02
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-05-06 13:04:59 +0530

    longest consecutive subsequence

--- Arrays/Medium/longestConsecutiveSubsequence.cpp
+++ Arrays/Medium/longestConsecutiveSubsequence.cpp
@@ -2,6 +2,8 @@
 
 // Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
 
+// better solution
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
@@ -39,5 +41,44 @@ class Solution{
             longest = max(longest, cnt);
         }
         return longest;
+    }
+};
+
+
+// optimal solution
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<unordered_set>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+    int findLongestConseqSubseq(int arr[], int N)
+    {
+      if (N == 0) return 0;
+      int longest = 1;
+      unordered_set<int> st;
+      for(int i=0; i<N; i++){
+          st.insert(arr[i]);
+      }
+      for(auto it: st){
+          if(st.find(it-1) == st.end()){
+              int cnt = 1;
+              int x = it;
+              while(st.find(x+1) != st.end()){
+                  x = x+1;
+                  cnt = cnt+1;
+              }
+              longest = max(longest, cnt);
+          }
+      }
+      return longest;
     }
 };
